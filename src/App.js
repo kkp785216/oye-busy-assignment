0e5aff333e0d962e3d581dcecc8320da06a9dd86
KRisHnA <kkp785216@gmail.com>
2022-09-01 16:34:28 +0530

    git-spa-setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename='oye-busy-assignment'>
+      <div className="App">
+        <Routes>
+          {/* <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="teams" element={<Teams />}>
+          <Route path=":teamId" element={<Team />} />
+          <Route path="new" element={<NewTeamForm />} />
+          <Route index element={<LeagueStandings />} />
+          </Route>
+          </Route> */}
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
