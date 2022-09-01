9c81548bb19e1cc9ca0535db094aba96e622dad7
KRisHnA <kkp785216@gmail.com>
2022-09-01 21:35:44 +0530

    header improved my account has dropdown and some work with homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,45 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import config from '../lib/config'
+import FmdGoodOutlinedIcon from '@mui/icons-material/FmdGoodOutlined';
+import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 const Header = () => {
-  return (
-    <header className="main-header">
-        <div className="main-header-wrapper">
-            <div className="main-header__left">
-                <Link to="/"><img src={`${config.basename}/assets/img/logo.svg`} className="logo" alt="LOGO"/></Link>
+    const { basename } = config;
+    return (
+        <header className="main-header">
+            <div className="main-header-wrapper">
+                <div className="main-header__left">
+                    <Link to="/"><img src={`${basename}/assets/img/logo.svg`} className="logo" alt="LOGO" /></Link>
+                    <div className="header__left__location">
+                        <FmdGoodOutlinedIcon />
+                        <span>1st Floor, H 356, Block B, Sector 22, Noida, Uttar Pradesh 201301</span>
+                        <KeyboardArrowDownOutlinedIcon fontSize='small' />
+                    </div>
+                </div>
+                <div className="main-header__right">
+                    <nav className="navbar">
+                        <ul>
+                            <li><Link to="/blog">Blog</Link></li>
+                            <li><Link to="/career">Career</Link></li>
+                            <li className='navbar__account'>
+                                <span>My Account</span><KeyboardArrowDownOutlinedIcon fontSize='small' />
+                                <div className="navbar__account__float">
+                                    <Link to="/profile">Profile</Link>
+                                    <Link to="/profile">Orders</Link>
+                                    <Link to="/profile">Cart</Link>
+                                    <Link to="/profile">Logout</Link>
+                                </div>
+                            </li>
+                            <li><Link to="/career"><ShoppingCartIcon className='navbar__shopicon' /></Link></li>
+                            <li><Link className='navbar__download' to="/career"><img src={`${basename}/assets/img/playstore 1.svg`} width='20' alt="" />Download App</Link></li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
-            <div className="main-header__right">
-                <nav className="navbar">
-                    <ul>
-                        <li><Link to="/blog">Blog</Link></li>
-                        <li><Link to="/career">Career</Link></li>
-                        <li><span>My Account</span></li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-    </header>
-  )
+        </header>
+    )
 }
 
 export default Header
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import config from '../lib/config'
 
 const Home = () => {
+  const { basename } = config;
   return (
-    <div>Home</div>
+    <div className="home">
+      <div className='home__top1'>
+        <div className="home__search__container" style={{ backgroundImage: `url('${basename}/assets/img/cosmetologist-cleaning-face-woman-beauty-salon (2) 1.jpg')` }}>
+          <h3>Home services, on demand</h3>
+          <form>
+            <SearchOutlinedIcon />
+            <input type="text" placeholder='Search for a service' />
+          </form>
+          <span>Examples: Salon, Women’s hair, Men’s grooming &amp; Many more..</span>
+        </div>
+        <div className="home__looking__container">
+
+        </div>
+      </div>
+    </div>
   )
 }
 
