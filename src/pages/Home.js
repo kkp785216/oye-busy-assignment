9ac7972aff997429a3b4316852244f0480c77268
KRisHnA <kkp785216@gmail.com>
2022-09-02 09:35:42 +0530

    home screen improved

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,13 +7,16 @@ const Home = () => {
   return (
     <div className="home">
       <div className='home__top1'>
-        <div className="home__search__container" style={{ backgroundImage: `url('${basename}/assets/img/cosmetologist-cleaning-face-woman-beauty-salon (2) 1.jpg')` }}>
-          <h3>Home services, on demand</h3>
-          <form>
-            <SearchOutlinedIcon />
-            <input type="text" placeholder='Search for a service' />
-          </form>
-          <span>Examples: Salon, Women’s hair, Men’s grooming &amp; Many more..</span>
+        <div className="home__search__container" style={{ /*backgroundImage: `url('${basename}/assets/img/cosmetologist-cleaning-face-woman-beauty-salon (2) 1.jpg')`*/ }}>
+          <img src={`${basename}/assets/img/cosmetologist-cleaning-face-woman-beauty-salon (2) 1.jpg`} width="100%" alt="" />
+          <div className="home__search__wrapper">
+            <h3>Home services, on demand</h3>
+            <form>
+              <SearchOutlinedIcon />
+              <input type="text" placeholder='Search for a service' />
+            </form>
+            <span>Examples: Salon, Women’s hair, Men’s grooming &amp; Many more..</span>
+          </div>
         </div>
         <div className="home__looking__container">
 
