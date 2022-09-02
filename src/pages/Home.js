d2cb7d692d55579f49d3e648f4803fbdfdd03df7
KRisHnA <kkp785216@gmail.com>
2022-09-02 13:50:18 +0530

    home__search and many more improvement

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import config from '../lib/config'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 const Home = () => {
   const { basename } = config;
@@ -20,9 +22,73 @@ const Home = () => {
           </div>
         </div>
         <div className="home__looking__container">
-          <div className="home__looking__heading">
-            <h3>What are you looking for?</h3>
-            <span>FLAT $400 OFF</span>
+          <div>
+            <div className="home__looking__heading">
+              <h3>What are you looking for?</h3>
+              <span>FLAT $400 OFF <img src={`${basename}/assets/img/Polygon 2.svg`} alt="" /></span>
+            </div>
+            <div className="home__looking__products">
+              <div className="home__looking__products__col">
+                <img src={`${basename}/assets/img/looking/Category_Salonlooking1.svg`} alt="" />
+                <span>Salon at Home</span>
+              </div>
+              <div className="home__looking__products__col">
+                <img src={`${basename}/assets/img/looking/Category_Electricianlooking2.svg`} alt="" />
+                <span>Electrician</span>
+              </div>
+              <div className="home__looking__products__col">
+                <img src={`${basename}/assets/img/looking/Category_Plumberlooking3.svg`} alt="" />
+                <span>Plumber</span>
+              </div>
+              <div className="home__looking__products__col">
+                <img src={`${basename}/assets/img/looking/Category_CCTVlooking4.svg`} alt="" />
+                <span>CCTV</span>
+              </div>
+              <div className="home__looking__products__col">
+                <img src={`${basename}/assets/img/looking/Category_AClooking5.svg`} alt="" />
+                <span>AC Service</span>
+              </div>
+              <div className="home__looking__products__col">
+                <img src={`${basename}/assets/img/looking/Category_Packer&Moverlooking6.svg`} alt="" />
+                <span>Packers &amp; Movers</span>
+              </div>
+              <div className="home__looking__products__col">
+                <img src={`${basename}/assets/img/looking/Category_Cleaninglooking7.svg`} alt="" />
+                <span>Cleaning</span>
+              </div>
+              <div className="home__looking__products__col">
+                <img src={`${basename}/assets/img/looking/Category_Painterlooking8.svg`} alt="" />
+                <span>House Painting</span>
+              </div>
+              <div className="home__looking__products__col">
+                <img src={`${basename}/assets/img/looking/Category_Home_Appliancelooking9.svg`} alt="" />
+                <span>Home Appliances</span>
+              </div>
+              <div className="home__looking__products__col">
+                <img src={`${basename}/assets/img/looking/Disinfectionlooking10.svg`} alt="" />
+                <span>Disinfection</span>
+              </div>
+              <div className="home__looking__products__col">
+                <img src={`${basename}/assets/img/looking/Category_Pest_controllooking11.svg`} alt="" />
+                <span>Pest Control</span>
+              </div>
+              <div className="home__looking__products__col">
+                <img src={`${basename}/assets/img/looking/Category_Carpenterlooking12.svg`} alt="" />
+                <span>Carpenter</span>
+              </div>
+            </div>
+          </div>
+          <div className="home__looking__swiper">
+            <Swiper
+              spaceBetween={11}
+              slidesPerView={2.4}
+            >
+              <SwiperSlide><img src={`${basename}/assets/img/looking/1.png`} width="100%" alt="" /></SwiperSlide>
+              <SwiperSlide><img src={`${basename}/assets/img/looking/2.png`} width="100%" alt="" /></SwiperSlide>
+              <SwiperSlide><img src={`${basename}/assets/img/looking/3.png`} width="100%" alt="" /></SwiperSlide>
+              <SwiperSlide><img src={`${basename}/assets/img/looking/4.png`} width="100%" alt="" /></SwiperSlide>
+              <SwiperSlide><img src={`${basename}/assets/img/looking/5.png`} width="100%" alt="" /></SwiperSlide>
+            </Swiper>
           </div>
         </div>
       </div>
