8f07832a751b222db44b85793cea415d758c39f8
KRisHnA <kkp785216@gmail.com>
2022-09-03 23:05:05 +0530

    more improvement in responsiveness

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 const ServiceCard = () => {
     return (
         <div className="servicecard">
-            <div className="servicecard__col">
+            <Swiper
+                spaceBetween={33}
+                slidesPerView={4}
+                breakpoints={{
+                    // 660: { slidesPerView: 3 },
+                    // 1010: { slidesPerView: 4 },
+                    // 1110: { slidesPerView: 5 },
+                    // 1260: { slidesPerView: 6 }
+                }}
+            >
+                {[1, 2, 3, 4].map((e, i) => (
+                    <SwiperSlide key={i}>
+                        <div className="servicecard__col">
+                            <div className="servicecard__img"></div>
+                            <strong>Professional Deep Cleaning</strong>
+                            <span>For spring  clean for  end of  lease clean</span>
+                        </div>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+            {/* <div className="servicecard__col">
                 <div className="servicecard__img"></div>
                 <strong>Professional Deep Cleaning</strong>
                 <span>For spring  clean for  end of  lease clean</span>
@@ -17,12 +39,7 @@ const ServiceCard = () => {
                 <div className="servicecard__img"></div>
                 <strong>Professional Deep Cleaning</strong>
                 <span>For spring  clean for  end of  lease clean</span>
-            </div>
-            <div className="servicecard__col">
-                <div className="servicecard__img"></div>
-                <strong>Professional Deep Cleaning</strong>
-                <span>For spring  clean for  end of  lease clean</span>
-            </div>
+            </div> */}
         </div>
     )
 }
