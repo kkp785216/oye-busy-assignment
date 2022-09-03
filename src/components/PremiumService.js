fc03f679391426a42da6a32afbc0d2aae5e03691
KRisHnA <kkp785216@gmail.com>
2022-09-03 10:24:46 +0530

    premium service designed with swiper js

--- src/components/PremiumService.js
+++ src/components/PremiumService.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -8,15 +8,19 @@ import { Navigation } from 'swiper';
 const PremiumService = ({ children }) => {
 
     // const { basename } = config;
+    const premiumRef=useRef();
+    useEffect(() => {
+        premiumRef.current.append(premiumRef.current.querySelector('.swiper-button-prev'));
+        premiumRef.current.append(premiumRef.current.querySelector('.swiper-button-next'));
+    }, []);
 
     return (
         <div className='premium__service'>
             <div className="premium__service__heading">
-                <h2>Plumber Services</h2>
+                <h2>Plumber<br/>Services</h2>
                 <span>Premium Services</span>
             </div>
-            <button className="swiper-button-prev">prev</button>
-            <div className="premium__service__swiper">
+            <div ref={premiumRef} className="premium__service__swiper">
                 <Swiper
                     spaceBetween={0}
                     slidesPerView={6}
